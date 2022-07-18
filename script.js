c4fb307a6eb4e9de515b6eeba38e4f42e079f8c8
Curtis Simpson <crsimpson5@gmail.com>
2022-07-17 20:54:35 -0500

    Add carousel functionality

--- script.js
+++ script.js
@@ -7,3 +7,43 @@ function toggleNav() {
 
 hamburgerBtn.addEventListener("click", toggleNav);
 closeBtn.addEventListener("click", toggleNav);
+
+const prevBtn = document.querySelector(".control-button--prev");
+const nextBtn = document.querySelector(".control-button--next");
+
+const hero = document.querySelector(".hero-image");
+
+let slide = 0;
+
+const heroImages = [
+  "url(../images/mobile-image-hero-1.jpg)",
+  "url(../images/mobile-image-hero-2.jpg)",
+  "url(../images/mobile-image-hero-3.jpg)"
+];
+
+prevBtn.addEventListener("click", prevSlide);
+nextBtn.addEventListener("click", nextSlide);
+
+function prevSlide() {
+  slide--;
+
+  if (slide < 0) {
+    slide = heroImages.length - 1;
+  }
+
+  setBackground();
+}
+
+function nextSlide() {
+  slide++;
+
+  if (slide > heroImages.length - 1) {
+    slide = 0;
+  }
+
+  setBackground();
+}
+
+function setBackground() {
+  hero.style.backgroundImage = heroImages[slide];
+}
